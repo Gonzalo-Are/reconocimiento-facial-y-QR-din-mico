859e1579807543aadd9bb33915cb27d2806b0f09
Agustin de la Vega <agustin.delavega02@gmail.com>
2025-04-16 19:05:36 -0400

    feat: agregar diseño de autenticación y eliminar encabezados innecesarios en las páginas de inicio de sesión y registro

--- src/app/(auth)/sign-in/[[...sign-in]]/page.js
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.js
@@ -3,7 +3,6 @@ import { SignIn } from "@clerk/nextjs";
 export default function Login() {
   return (
     <div>
-      <h1>Sign-in Page</h1>
       <SignIn/>
     </div>
   );
--- src/app/(auth)/sign-up/[[...sign-up]]/page.js
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.js
@@ -3,7 +3,6 @@ import { SignUp } from "@clerk/nextjs";
 export default function SignUpPage() {
   return (
     <div>
-      <h1>Sign Up Page</h1>
       <SignUp />
     </div>
   );
