654ca7ac7cce3d02e8643d86c343b4de7ceafe72
Gonzalo-Are <109882199+Gonzalo-Are@users.noreply.github.com>
2025-04-24 16:28:05 -0400

    SUbida

---
+++ src/app/(routes)/(dashboard)/settings/page.js
@@ -0,0 +1,13 @@
+import UploadPhotoForm from "@/components/UploadPhotoForm"
+
+export default function DashboardSettingsPage() {
+  return (
+    <div className="p-8 space-y-6">
+      <h1 className="text-2xl font-bold">Ajustes de Cuenta</h1>
+      <section className="bg-white p-6 rounded-lg shadow">
+        <h2 className="text-xl mb-4">Foto de Perfil</h2>
+        <UploadPhotoForm />
+      </section>
+    </div>
+  )
+}
