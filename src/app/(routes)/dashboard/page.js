d127589a0e41a5b927c1fa46b1f4dce7d4e178ff
woldejoaquin <woldejoaquin@gmail.com>
2025-04-24 15:11:40 -0400

    Creación de QR por usuario

--- src/app/(routes)/dashboard/page.js
+++ src/app/(routes)/dashboard/page.js
@@ -1,7 +1,28 @@
+import { SignedIn, SignedOut } from '@clerk/nextjs'
+import QRUsuario from '../../../components/ui/QRUsuario'
+import Link from 'next/link'
+
 export default function Dashboard() {
     return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
-    );
+        <>
+          <SignedIn>
+            <div className="p-6">
+              <h1 className="text-2xl font-bold mb-4">Panel de Control</h1>
+              <QRUsuario />
+            </div>
+          </SignedIn>
+    
+          <SignedOut>
+            <div className="p-6 text-center text-gray-600">
+              <p>
+              Por favor,{' '}
+            <Link href="/sign-in" className="text-blue-500 underline">
+              inicia sesión
+            </Link>{' '}
+            para ver tu código QR.
+              </p>
+            </div>
+          </SignedOut>
+        </>
+      )
 }
---
+++ src/app/api/generarQR/route.js
@@ -0,0 +1,21 @@
+// src/app/api/ganerarQR/route.js
+import { NextResponse } from 'next/server'
+import jwt from 'jsonwebtoken'
+
+export async function GET(request) {
+  const { searchParams } = new URL(request.url)
+  const userId = searchParams.get('userId')
+
+  if (!userId) {
+    return NextResponse.json({ error: 'Falta userId' }, { status: 400 })
+  }
+
+  // Genera un token que expira en 1 minuto
+  const token = jwt.sign(
+    { userId },
+    process.env.JWT_SECRET,
+    { expiresIn: '1m' }
+  )
+
+  return NextResponse.json({ token })
+}
